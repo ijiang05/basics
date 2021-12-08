3261fd1fa979554424fba4a5bc87c3b9653307ca
Kylie J <ijiang05@gmail.com>
2021-12-08 17:36:51 -0500

    hi

---
+++ Gitbasics.java
@@ -0,0 +1,15 @@
+public class Gitbasics {
+    public static void main(String[] args){
+        pow2();
+    }
+        public static void pow2(){
+            int x = 1;
+            while(x<694){
+                System.out.println(x);
+                x*= 2;
+            }
+               
+        }
+        
+    }
+
